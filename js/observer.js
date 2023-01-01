33f799ea7ed6487a5ee15473d5287e17db9aeb63
unknown <muhammedadam4544@gmail.com>
2023-01-01 22:00:44 +0100

    desktop responsiveness css added

--- js/observer.js
+++ js/observer.js
@@ -1,13 +1,12 @@
 console.log("Hello");
-let header = document.querySelector('.container');
+let header = document.querySelector(".container");
 
-document.addEventListener('scroll',()=>{
-    let scrollTop = window.scrollY;
-    if(scrollTop >200 ){
-        header.style.backgroundColor = 'transparent'; 
-    }else{
-        header.style.backgroundColor = 'transparent'
-    }
+// document.addEventListener('scroll',()=>{
+//     let scrollTop = window.scrollY;
+//     if(scrollTop >200 ){
+//         header.style.backgroundColor = 'wh';
+//     }else{
+//         header.style.backgroundColor = 'transparent'
+//     }
 
-
-})
+// })
