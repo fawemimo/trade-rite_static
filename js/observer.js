009bf798df1c87dc07b7bdcfd90acbf33dcb7943
unknown <muhammedadam4544@gmail.com>
2023-01-05 17:36:31 +0100

    responsiveness done

--- js/observer.js
+++ js/observer.js
@@ -1,5 +1,4 @@
-console.log("Hello");
-let header = document.querySelector(".container");
+// let header = document.querySelector(".container");
 
 // document.addEventListener('scroll',()=>{
 //     let scrollTop = window.scrollY;
@@ -10,3 +9,37 @@ let header = document.querySelector(".container");
 //     }
 
 // })
+
+// Tab
+
+const tabButtons = [...document.querySelectorAll(".easy-steps-tab > button")];
+const tabContent = [
+	...document.querySelectorAll(".easy-steps-tab-container > p"),
+];
+
+let activeIndex = 0;
+
+function showCurrentTab() {
+	tabContent.forEach((tab, i, arr) => {
+		tab.style.display = "none";
+		arr[activeIndex].style.display = "block";
+	});
+}
+
+window.addEventListener("load", () => {
+	tabContent.forEach((tab) => {
+		tab.style.display = "none";
+	});
+	tabButtons.forEach((btn, i) => {
+		btn.setAttribute("data-index", i);
+	});
+	showCurrentTab();
+});
+
+tabButtons.forEach((btn) => {
+	btn.addEventListener("click", () => {
+		activeIndex = btn.dataset.index;
+		showCurrentTab();
+		console.log(activeIndex);
+	});
+});
